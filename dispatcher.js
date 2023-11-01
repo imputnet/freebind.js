cdebd6616073f6f8c8e0e889edb0778f7f75ee5b
dumbmoron <log@riseup.net>
2023-11-01 23:38:36 +0000

    fix indentation

--- dispatcher.js
+++ dispatcher.js
@@ -3,29 +3,29 @@ import { Agent, buildConnector } from 'undici'
 
 // taken from `fetch-socks`, many thanks
 function resolvePort(protocol, port) {
-	return port ? Number.parseInt(port) : protocol === "http:" ? 80 : 443
+    return port ? Number.parseInt(port) : protocol === "http:" ? 80 : 443
 }
 
 export function createRandomConnector(cidr, bits, tlsOpts = {}) {
-	const undiciConnect = buildConnector(tlsOpts)
+    const undiciConnect = buildConnector(tlsOpts)
 
-	return async (options, callback) => {
-		let { protocol, hostname, port } = options;
+    return async (options, callback) => {
+        let { protocol, hostname, port } = options;
 
-		return undiciConnect({
+        return undiciConnect({
             ...options,
             httpSocket: await createRandomSocket(
                 hostname,
                 resolvePort(protocol, port),
                 cidr,
-				undefined,
-				bits
+                undefined,
+                bits
             )
         }, callback);
-	};
+    };
 }
 
 export function randomDispatcher(cidr, options = {}) {
-	const { connect, bits, ...rest } = options
-	return new Agent({ ...rest, connect: createRandomConnector(cidr, bits, connect) })
+    const { connect, bits, ...rest } = options
+    return new Agent({ ...rest, connect: createRandomConnector(cidr, bits, connect) })
 }
