e5f24ea2f19d0f93d571539cda4b10aa71fd0451
dumbmoron <log@riseup.net>
2024-05-12 11:54:44 +0000

    add createSocket method for manually specifying IP

--- dispatcher.js
+++ dispatcher.js
@@ -1,9 +1,34 @@
-import { createRandomSocket } from './socket.js'
+import { generateRandomIP } from './ip.js';
+import { createRandomSocket, createSocketFromHostname } from './socket.js'
 import { Agent, buildConnector } from 'undici'
 
 // taken from `fetch-socks`, many thanks
 function resolvePort(protocol, port) {
     return port ? Number.parseInt(port) : protocol === "http:" ? 80 : 443
+}
+
+export function createConnectorFromIP(ip, tlsOpts = {}, sockOpts = {}) {
+    const undiciConnect = buildConnector(tlsOpts)
+
+    return async (options, callback) => {
+        let { protocol, hostname, port } = options;
+
+        return undiciConnect({
+            ...options,
+            httpSocket: await createSocketFromHostname(
+                hostname,
+                resolvePort(protocol, port),
+                ip,
+                sockOpts
+            )
+        }, callback);
+    };
+}
+
+export function createRandomStickyConnector(cidr, tlsOpts, options) {
+    const { bits, ...sockOpts } = options;
+    const ip = generateRandomIP(cidr, bits);
+    return createConnectorFromIP(ip, tlsOpts, sockOpts);
 }
 
 export function createRandomConnector(cidr, tlsOpts = {}, sockOpts = {}) {
@@ -24,7 +49,18 @@ export function createRandomConnector(cidr, tlsOpts = {}, sockOpts = {}) {
     };
 }
 
+export function dispatcherFromIP(ip, options = {}) {
+    const { connect, ...rest } = options
+    return new Agent({ ...rest, connect: createConnectorFromIP(ip, connect, rest) })    
+}
+
+export function randomStickyDispatcher(cidr, options = {}) {
+    const { connect, ...rest } = options
+    return new Agent({ ...rest, connect: createRandomStickyConnector(cidr, connect, rest) })
+}
+
 export function randomDispatcher(cidr, options = {}) {
     const { connect, ...rest } = options
     return new Agent({ ...rest, connect: createRandomConnector(cidr, connect, rest) })
 }
+
